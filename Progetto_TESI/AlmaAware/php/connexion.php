bd32f6b70dae0cbc3aa4b5e284a951e9246354b2
Anna Bartolucci <anna.bartolucci@studio.unibo.it>
2024-04-17 12:16:59 +0200

    implemented connexion page

---
+++ Progetto_TESI/AlmaAware/php/connexion.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/connexion_style.css" >
+    
+
+    <title>AlmAware - Connexion</title>
+    <link rel="shortcut icon" href="../images/medias/Mascotte.svg">
+</head>
+<body>
+<main>
+    <div class="header">
+      <h1>AlmAware</h1>
+    </div>
+      
+      <div class="footer sdgLine">
+        <a href="../php/login.php"><Button class="btn-unibo" >Sign in</Button></a>
+        <a href="../php/registration.php"><Button class="btn-unibo-outline" >Sign up</Button></a>
+      </div>
+
+  </main>
+</body>
+</html>
+
